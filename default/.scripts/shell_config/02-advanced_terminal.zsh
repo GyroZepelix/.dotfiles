58612e5d6539dada2064dac938d354f3ad6ddde1
GyroZepelix <gamingskullgamer@gmail.com>
2025-11-05 10:28:33 +0100

    feat: moved task and timew aliases to deprecated

--- default/.scripts/shell_config/02-advanced_terminal.zsh
+++ default/.scripts/shell_config/02-advanced_terminal.zsh
@@ -17,17 +17,6 @@ alias dcu="docker compose up -d"
 alias dcd="docker compose down"
 alias dcl="docker compose logs"
 
-# Taskwarrior and timewarrior aliases
-alias t="task"
-alias tw="timew"
-alias tws="timew summary"
-alias twp="timew stop"
-alias twc="timew continue"
-alias twcl="timew continue @1"
-alias twms="timew summary :month :ids"
-alias twm="timew month"
-alias twml="timew month :lastmonth"
-
 # Neovim
 alias n="nvim"
 
---
+++ deprecated/.scripts/shell_config/d2-advanced-terminal-timew.zsh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Taskwarrior and timewarrior aliases
+alias t="task"
+alias tw="timew"
+alias tws="timew summary"
+alias twp="timew stop"
+alias twc="timew continue"
+alias twcl="timew continue @1"
+alias twms="timew summary :month :ids"
+alias twm="timew month"
+alias twml="timew month :lastmonth"
